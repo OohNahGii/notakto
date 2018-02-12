62ad80c1dd8d7c2cf6f2db959ce6e4f0cd9a8ece
Grant Ramil <grant.ramil@veeva.com>
2018-02-11 20:10:32 -0800

    Initial commit. Still need to refactor board.js to only expose the necessary methods.

---
+++ board.js
@@ -0,0 +1,109 @@
+function Board(num, length) {
+  this.num = num;
+  this.length = length;
+  this.state = [];
+  // initalize state
+  for (let index = 0; index < length * length; index++) {
+    this.state.push('');
+  }
+  
+  this.is_board_dead = function() {
+    return this.has_horizontal() || this.has_vertical() || this.has_diagonal();
+  }
+
+  this.has_horizontal = function() {
+    for (let index = 0; index < this.length * this.length - 1; index = index + this.length) {
+      const row = this.state.slice(index, index + this.length);
+      if (this.is_slice_dead(row)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  this.has_vertical = function() {
+    for (let index = 0; index < this.length; index++) {
+      let column = [];
+      for (let row = index; row < this.state.length; row = row + this.length) {
+        column.push(this.state[row]);
+      }
+      if (this.is_slice_dead(column)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  this.has_diagonal = function() {
+    const leftDistance = this.length + 1;
+    const rightDistance = this.length - 1;
+    
+    // top-left to bottom-right
+    let topLeft = [];
+    for (let index = 0; index < this.state.length; index = index + leftDistance) {
+      topLeft.push(this.state[index]);
+    }
+    if (this.is_slice_dead(topLeft)) {
+      return true;
+    }
+
+    // top-right to bottom-left
+    let topRight = [];
+    for (let index = rightDistance; index < this.state.length - 1; index = index + rightDistance) {
+      topRight.push(this.state[index]);
+    }
+    if (this.is_slice_dead(topRight)) {
+      return true;
+    }
+    return false;
+  }
+
+  this.is_slice_dead = function(slice) {
+    return slice.every(cell => {
+      return cell;
+    });
+  }
+
+  this.print_board = function() {
+    // Print board name
+    let boardName = 'Board ' + this.num;
+    if (this.is_board_dead()) {
+      boardName = boardName + ' (DEAD)';
+    }
+    process.stdout.write(boardName + '\n');
+
+    // Print board
+    const cellSize = 3; // eg. ' X '
+    let separatorLength = this.length * cellSize + (this.length - 1);
+    for (let index = 0; index < this.state.length; index++) {
+      if (index > 0 && index % length == 0) {
+        process.stdout.write('-'.repeat(separatorLength) + '\n');
+      }
+      const cellValue = this.state[index] ? this.state[index] : index;
+      process.stdout.write(' ' + cellValue + ' ');
+      if ((index + 1) % length != 0) {
+        process.stdout.write('|');
+      } else {
+        process.stdout.write('\n');
+      }
+    }
+    process.stdout.write('\n');
+  }
+
+  this.update_board = function(cellNum) {
+    if (this.is_move_valid(cellNum)) {
+      this.state[cellNum] = 'X';
+      return true;
+    }
+    return false;
+  }
+
+  this.is_move_valid = function(cellNum) {
+    if (this.is_board_dead()) {
+      return false;
+    }
+    return !this.state[cellNum];
+  }
+}
+
+module.exports = Board;
---
+++ notakto.js
@@ -0,0 +1,77 @@
+const Board = require('./board.js');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function initialize_boards(numBoards, boardLength) {
+  let boards = [];
+  for (let count = 0; count < numBoards; count++) {
+    boards.push(new Board(count, boardLength));
+  }
+  return boards;
+}
+
+function perform_turn(boards, currentPlayer, player1, player2) {
+  const next_player = get_next_player(currentPlayer, player1, player2);
+  print_boards(boards);
+  if (is_game_over(boards)) {
+    console.log('Winner is ' + next_player);
+    rl.close();
+    return;
+  }
+
+  rl.question(currentPlayer + ' enter move: ', (answer) => {
+    if (update_boards(boards, answer)) {
+      return perform_turn(boards, next_player, player1, player2);
+    } else {
+      console.log('Move [' + answer + '] is invalid.\n');
+      return perform_turn(boards, currentPlayer, player1, player2);
+    }
+  });
+}
+
+function is_game_over(boards) {
+  return boards.every((board) => {
+    return board.is_board_dead();
+  });
+}
+
+function print_boards(boards) {
+  boards.forEach((board) => {
+    board.print_board();
+  });
+}
+
+function update_boards(boards, input) {
+  const values = input.split('-', 2);
+  if (values.length != 2) {
+    return false;
+  }
+  if (Number.isNaN(values[0]) || Number.isNaN(values[1])) {
+    return false;
+  }
+  const boardNum = Number.parseInt(values[0], 10);
+  const cellNum = Number.parseInt(values[1], 10);
+  if (boardNum < 0 || boardNum >= boards.length) {
+    return false;
+  }
+  const boardLength = boards[0].length;
+  const numCellsInBoard = boardLength * boardLength;
+  if (cellNum < 0 || cellNum >= numCellsInBoard) {
+    return false;
+  }
+  return boards[boardNum].update_board(cellNum);
+}
+
+function get_next_player(current, player1, player2) {
+  return current.name === player1.name ? player2 : player1;
+}
+
+const player1 = 'Player 1';
+const player2 = 'Player 2';
+const boardLength = 3;
+const numBoards = 3;
+let boards = initialize_boards(numBoards, boardLength);
+perform_turn(boards, player1, player1, player2);
